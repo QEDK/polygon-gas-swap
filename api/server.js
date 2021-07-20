7836d372dc6efe4725780127c13d1fbf4a17eb31
QEDK <1994constant@gmail.com>
2021-07-20 14:22:07 +0530

    Add endpoint

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config({ path: '../.env' })
+const axios = require('axios')
+const qs = require('qs')
 // Require the framework and instantiate it
 const fastify = require('fastify')({
   logger: true
@@ -14,7 +16,22 @@ fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
 })
 
-fastify.get('/transfer', (request, reply) => {
+fastify.get('/quote', async (request, reply) => {
+  const params = {
+    buyToken: 'MATIC',
+    sellToken: 'DAI',
+    buyAmount: 10000000000000000000
+  }
+  const response = await axios({
+    method: 'GET',
+    url: 'https://polygon.api.0x.org/swap/v1/quote',
+    params: params
+  })
+  reply.send(response.data)
+})
+
+
+fastify.get('/transfer', async (request, reply) => {
   try {
     if (request.body.apiKey !== process.env.API_KEY) {
       reply.statusCode = 403
