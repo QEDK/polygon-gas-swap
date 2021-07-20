29ee07bd45beaf0234fae84215c741d6bbea20df
QEDK <1994constant@gmail.com>
2021-07-20 17:11:42 +0530

    Add method calls

--- api/server.js
+++ api/server.js
@@ -1,10 +1,7 @@
 require('dotenv').config({ path: '../.env' })
 const axios = require('axios')
 const Web3 = require('web3')
-const ERC20 = require('./config/erc20.json')
-const fastify = require('fastify')({
-  logger: true
-})
+const ERC20ABI = require('./config/erc20.json')
 
 if (!process.env.API_KEY) {
   console.error('API key is undefined!')
@@ -15,14 +12,19 @@ if (!process.env.API_KEY) {
 }
 
 const web3 = new Web3(process.env.RPC_URL || 'https://rpc-mainnet.matic.quiknode.pro')
-var account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
-web3.eth.accounts.wallet.add(account);
+const account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
+web3.eth.accounts.wallet.add(account)
+
+const fastify = require('fastify')({
+  logger: true
+})
+fastify.register(require('fastify-formbody'))
 
 fastify.get('/', (request, reply) => {
   reply.send('Polygon Gas Swap API')
 })
 
-fastify.get('/quote', async (request, reply) => {
+fastify.post('/quote', async (request, reply) => {
   try {
     if (request.body.apiKey !== process.env.API_KEY) {
       reply.statusCode = 403
@@ -31,14 +33,25 @@ fastify.get('/quote', async (request, reply) => {
     const params = {
       buyToken: 'MATIC',
       sellToken: 'DAI',
-      buyAmount: 100000000000000000
+      buyAmount: 1000000000000000000
     }
     const response = await axios({
       method: 'GET',
       url: 'https://polygon.api.0x.org/swap/v1/quote',
       params: params
     })
-    reply.send(response.data)
+    console.log(response.data)
+    const daiContract = new web3.eth.Contract(ERC20ABI, response.data.sellTokenAddress)
+    console.log(daiContract)
+    const tx = await daiContract.methods.approve(response.data.allowanceTarget, response.data.sellAmount).send({
+      from: account.address,
+      gas: 100000,
+      gasPrice: response.data.gasPrice
+    })
+    console.log(tx)
+    const tx2 = await web3.sendTransaction(response.data)
+    console.log(tx2)
+    reply.send()
   } catch (err) {
     console.error(err)
     reply.statusCode = 500
