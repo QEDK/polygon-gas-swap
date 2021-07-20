b77166807d6ef34450d715b306bb4155c8c5b5f5
QEDK <1994constant@gmail.com>
2021-07-20 15:58:25 +0530

    Add account logic

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../.env' })
 const axios = require('axios')
-const qs = require('qs')
-// Require the framework and instantiate it
+const Web3 = require('web3')
+const ERC20 = require('./config/erc20.json')
 const fastify = require('fastify')({
   logger: true
 })
@@ -9,25 +9,44 @@ const fastify = require('fastify')({
 if (!process.env.API_KEY) {
   console.error('API key is undefined!')
   process.exit(1)
+} else if (!process.env.PRIVATE_KEY) {
+  console.error('Private key is undefined!')
+  process.exit(1)
 }
 
-// Declare a route
+const web3 = new Web3(process.env.RPC_URL || 'https://rpc-mainnet.matic.quiknode.pro')
+var account = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
+web3.eth.accounts.wallet.add(account);
+
 fastify.get('/', (request, reply) => {
-  reply.send({ hello: 'world' })
+  reply.send('Polygon Gas Swap API')
 })
 
 fastify.get('/quote', async (request, reply) => {
-  const params = {
-    buyToken: 'MATIC',
-    sellToken: 'DAI',
-    buyAmount: 10000000000000000000
+  try {
+    if (request.body.apiKey !== process.env.API_KEY) {
+      reply.statusCode = 403
+      return
+    }
+    const params = {
+      buyToken: 'MATIC',
+      sellToken: 'DAI',
+      buyAmount: 100000000000000000
+    }
+    const response = await axios({
+      method: 'GET',
+      url: 'https://polygon.api.0x.org/swap/v1/quote',
+      params: params
+    })
+    reply.send(response.data)
+  } catch (err) {
+    console.error(err)
+    reply.statusCode = 500
+  } finally {
+    if(!reply.sent) {
+      reply.send()
+    }
   }
-  const response = await axios({
-    method: 'GET',
-    url: 'https://polygon.api.0x.org/swap/v1/quote',
-    params: params
-  })
-  reply.send(response.data)
 })
 
 
@@ -35,6 +54,7 @@ fastify.get('/transfer', async (request, reply) => {
   try {
     if (request.body.apiKey !== process.env.API_KEY) {
       reply.statusCode = 403
+      return
     }
   } catch (err) {
     console.error(err)
