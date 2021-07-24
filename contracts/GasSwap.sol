4103fa37f412635ad0c77ffdef42b9e81ffc0754
QEDK <1994constant@gmail.com>
2021-07-24 06:46:56 +0530

    :new: Update contracts

--- contracts/GasSwap.sol
+++ contracts/GasSwap.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-// A partial ERC20 interface.
+import "./EIP712MetaTransaction.sol";
+
+struct Transformation {
+    uint32 _uint32;
+    bytes _bytes;
+}
+
 interface IERC20 {
-    function balanceOf(address owner) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
 }
 
-contract GasSwap  {
+contract GasSwap is EIP712MetaTransaction("GasSwap", "1") {
     address public owner;
 
     constructor() {
@@ -21,10 +26,8 @@ contract GasSwap  {
         _;
     }
 
-    // Payable fallback to allow this contract to receive protocol fee refunds.
     receive() external payable {}
 
-    // Transfer tokens held by this contrat to the sender/owner.
     function withdrawToken(IERC20 token, uint256 amount)
         external
         onlyOwner
@@ -32,43 +35,44 @@ contract GasSwap  {
         require(token.transfer(msg.sender, amount));
     }
 
-    // Transfer ETH held by this contrat to the sender/owner.
+    // Transfer ETH held by this contract to the sender/owner.
     function withdrawETH(uint256 amount)
         external
         onlyOwner
     {
-        msg.sender.transfer(amount);
+        payable(msg.sender).transfer(amount);
     }
 
-    // Transfer ETH into this contract and wrap it into WETH.
-    receive() external payable;
-
     // Swaps ERC20->MATIC tokens held by this contract using a 0x-API quote.
-    function fillQuote(
-        // Address of token being sold
-        IERC20 sellToken,
-        // Amount of token being sold
-        uint256 sellAmount,
-        // Amount of MATIC being bought
-        uint256 buyAmount,
-        // Address sending token address for swap
-        address payable depositor,
-        // Address that needs approval to spend our tokens
-        address spender,
-        address payable swapTarget,
-        bytes calldata swapCallData
-    )
-        onlyOwner
-        external
+    function fillQuote(address spender, address swapTarget, bytes calldata swapCallData) payable external returns (uint256)
     {
-        require(sender.balance < 1000000000000000000, "SENDER_BALANCE_EXCEEDS_LIMIT");
-        require(sellToken.transferFrom(depositor, address(this), amount), "TRANSFER_FAILED");
-        // required for some tokens like USDT
+        require(msgSender().balance <= 1000000000000000000, "SENDER_BALANCE_EXCEEDS_LIMIT");
+        (address inputToken,address outputToken,uint256 inputAmount,uint256 minOutputAmount,) = abi.decode(swapCallData[4:], (address,address,uint256,uint256,Transformation[]));
+        require(outputToken == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE, "INVALID_OUTPUT_TOKEN");
+        IERC20 sellToken = IERC20(inputToken);
+        require(sellToken.transferFrom(msgSender(), address(this), inputAmount), "TRANSFER_FAILED");
         require(sellToken.approve(spender, uint256(0)), "APPROVAL_WIPE_FAILED");
-        require(sellToken.approve(spender, amount), "REAPPROVAL_FAILED");
+        require(sellToken.approve(spender, inputAmount), "REAPPROVAL_FAILED");
+        (bool success, bytes memory res) = swapTarget.call{value: msg.value}(swapCallData);
+        uint256 outputTokenAmount = abi.decode(res, (uint256));
+        require(success, string(concat(bytes("SWAP_FAILED: "),bytes(getRevertMsg(res)))));
+        require(outputTokenAmount >= minOutputAmount, "SWAP_VALUE_MISMATCH");
+        payable(msgSender()).transfer(outputTokenAmount);
+        return outputTokenAmount;
+    }
 
-        (bool success,) = swapTarget.call{value: msg.value}(swapCallData);
-        require(success, 'SWAP_CALL_FAILED');
-        depositor.transfer(buyAmount);
+    function concat(bytes memory a, bytes memory b) internal pure returns (bytes memory) {
+        return abi.encodePacked(a, b);
+    }
+
+    function getRevertMsg(bytes memory _returnData) internal pure returns (string memory) {
+        if (_returnData.length < 68)
+            return "Transaction reverted silently";
+
+        assembly {
+            _returnData := add(_returnData, 0x04)
+        }
+
+        return abi.decode(_returnData, (string));
     }
 }
